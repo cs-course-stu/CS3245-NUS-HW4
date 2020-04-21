208eb0ae184ed5a797d6e5783f10e9c4cdc8287d
Jothi Pillay <jothipillay1996@gmail.com>
2020-04-21 18:07:27 +0800

    read in csv files

--- src/index.py
+++ src/index.py
@@ -3,9 +3,18 @@ import re
 import nltk
 import sys
 import getopt
+import csv
+DOC_ID = 1
+TITLE = 2
+CONTENT = 3
+DATE = 4
+COURT = 5
+
+
 
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
+
 
 def build_index(in_dir, out_dict, out_postings):
     """
@@ -13,8 +22,31 @@ def build_index(in_dir, out_dict, out_postings):
     then output the dictionary file and postings file
     """
     print('indexing...')
-    # This is an empty method
-    # Pls implement your code in below
+
+    # nested dictionary to store dataset values
+    dataset_dictionary = {}
+    print(dataset_dictionary)
+    print("\n")
+
+    # read input data from csv files
+    with open('testinput.csv', mode='r') as input_csv_file:
+        csv_reader = csv.reader(input_csv_file, delimiter=',', quotechar='"')
+        line_num = 0
+        for line in csv_reader:
+
+            # if not header columns, read into dictionary
+            if line_num != 0:
+                doc_id = line["document_id"]
+                dataset_dictionary[doc_id] = {}
+                dataset_dictionary[doc_id][TITLE] = line["title"]
+                dataset_dictionary[doc_id][CONTENT] = line["content"]
+                dataset_dictionary[doc_id][DATE] = line["date_posted"]
+                dataset_dictionary[doc_id][COURT] = line["court"]
+
+    print(dataset_dictionary)
+    input_csv_file.close()
+    print('indexing completed')
+
 
 input_directory = output_file_dictionary = output_file_postings = None
 
@@ -33,6 +65,7 @@ for o, a in opts:
         output_file_postings = a
     else:
         assert False, "unhandled option"
+
 
 if input_directory == None or output_file_postings == None or output_file_dictionary == None:
     usage()
