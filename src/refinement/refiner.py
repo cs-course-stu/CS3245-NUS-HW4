8255ae3db8c78ff4b4557b1d03b6ad2305407f30
xjdkc <xjdkcsq3@gmail.com>
2020-04-24 17:15:15 +0000

    feat: add relevance feedback

--- src/refinement/refiner.py
+++ src/refinement/refiner.py
@@ -6,7 +6,7 @@ import nltk
 import array
 import heapq
 import numpy as np
-# from indexer import Indexer
+from indexer import Indexer
 from nltk.corpus import stopwords
 from collections import defaultdict
 from nltk.stem.porter import PorterStemmer
@@ -22,20 +22,31 @@ class QueryInfo:
         self.counts = None
         self.tokens = None
 
-
 class Refiner:
 
-    def __init__(self, indexer=None, alpha=0.1, beta=0.1,
-                 expand=False, feedback=False):
+    """ Refiner is a class to preprocess and define the boolean query.
+        It divides the boolean query string into separate queries and
+        uses the approaches of Query Expansion and Relevance Feedback
+        to refine them.
+
+    Args:
+        indexer: an instance of Indexer used to load the index
+        expand: boolean indicator for using Query Expansion
+        feedback: boolean indicator for using Relevance Feedback
+        alpha: a coefficient of Relevance Feedback
+        beta: a coefficient of Releance Feedback
+    """
+    def __init__(self, indexer=None, expand=False, feedback=True,
+                 alpha=0.5, beta=0.5):
         self.indexer = indexer
-        self.alpha = alpha
-        self.beta = beta
         self.expand = expand
         self.feedback = feedback
+        self.alpha = alpha
+        self.beta = beta
 
         self.stemmer = PorterStemmer()
 
-    """ Refine the boolean query based on Relevant Feedback and Query Expansion
+    """ Refine the boolean query based on Relevance Feedback and Query Expansion
 
     Args:
         query: the boolean query string
@@ -43,27 +54,30 @@ class Refiner:
 
     Returns:
         query_infos: the list of query info for separate query
-        postings_lists: the dictionary with terms to posting lists mapping
+        postings_lists: the dictionary with terms to postings lists mapping
     """
     def refine(self, query, relevant_docs):
-        # step 1: split the boolean query into single query
-        query_infos, total_terms = self.split_query(self, query)
+        # step 1: split the boolean query into separate queries
+        query_infos = self._split_query(query)
 
-        # step 2: get the postings lists of the terms
-        # postings_lists = self.indexer.LoadTerms(total_terms)
-
-        # step 3: extend all the single query
+        # step 2: expand all the single queries
         if self.expand:
             self._expand(query_infos)
 
-        # step 4: construct query vector
-        self._get_query_vector(query_infos)
+        # step 3: tokenize all the single queries
+        total_terms = self._tokenize(query_infos)
 
-        # step 5: perform Relevant Feedback
+        # step 4: get the postings lists of total terms
+        postings_lists = self.indexer.LoadTerms(total_terms)
+
+        # step 5: construct query vector
+        self._get_query_vector(query_infos, postings_lists)
+
+        # step 6: perform Relevance Feedback
         if self.feedback:
-            self._feedback(query_infos, relevant_docs)
+            self._feedback(query_infos, relevant_docs, postings_lists)
 
-        # step 6: return query info and the postings lists
+        # step 7: return query info and the postings lists
         return query_infos, postings_lists
 
     """ Split the boolean query string into seprate quries
@@ -73,12 +87,10 @@ class Refiner:
 
     Returns:
         query_infos: a list of instances of QueryInfo
-        total_terms: all terms in the boolean query
     """
 
     def _split_query(self, query):
         query_infos = []
-        total_terms = set()
 
         # step 1: split boolean query string based on 'AND'
         queries = query.split('AND')
@@ -101,71 +113,101 @@ class Refiner:
             else:
                 query_info = QueryInfo(q)
 
-            print(query_info.query)
-
-            # step 3: tokenize every single query
-            self._tokenize(query_info)
-
-            # step 4: append the query infos
+            # step 3: append the query infos
             query_infos.append(query_info)
-            total_terms.update(query_info.terms)
 
         # step 4: return query_infos
-        return query_infos, total_terms
+        return query_infos
 
-    def _expend(self, query_infos):
-        # step 1: expend every single query by using wordnet
-
-        pass
-
-    def _feedback(self, query_infos, relevant_docs):
-        # step 1: get doc vectors of the relevant docs
-
-        # step 2: perform feedback for every single query
+    def _expand(self, query_infos):
+        # step 1: expand every single query by using wordnet
 
         pass
 
-    """ Tokenize the query in the query_info
+
+    """ Perform Relevance Feedback based on Rocchio Algorithm
 
     Args:
-        query_info: an instance of QueryInfo that contains the query
+        query_infos: a list of instances of QueryInfo
+        relevant_docs: the list of relevant docs
+        postings_lists: the dictionary with terms to postings lists mapping
     """
+    def _feedback(self, query_infos, relevant_docs, postings_lists):
+        for query_info in query_infos:
+            terms = query_info.terms
+            query_info.query_vector *= self.alpha
+            query_vector = query_info.query_vector
 
-    def _tokenize(self, query_info):
-        query = query_info.query
+            print(query_vector)
 
-        # step 1: tokenize the query string
-        tokens = [
-            word for sent in nltk.sent_tokenize(query)
-            for word in nltk.word_tokenize(sent)
-        ]
+            # step 1: perform relevance feedback for every query
+            for i, term in enumerate(terms):
+                postings_list = postings_lists[term]
+                postings = postings_list[0]
+                weights = postings_list[1]
 
-        # step 2: stem the tokens
-        tokens = [self.stemmer.stem(token.lower()) for token in tokens]
+                weight = 0
+                for j in range(0, len(postings)):
+                    doc = postings[j]
 
-        # step 3: get the term count
-        term_count = defaultdict(lambda: 0)
-        for token in tokens:
-            term_count[token] += 1
+                    if doc not in relevant_docs:
+                        continue
 
-        # step 4: get terms and counts
-        terms = []
-        counts = []
-        for term in term_count:
-            terms.append(term)
-            counts.append(term_count[term])
+                    weight += weights[j]
 
-        # step 5: update query info
-        query_info.terms = terms
-        query_info.counts = counts
-        query_info.tokens = tokens
+                query_vector[i] += self.beta * weight
 
+    """ Tokenize the all the queries in the query_infos
+
+    Args:
+        query_infos: a list of instances of QueryInfo that contains the query
+
+    Returns:
+        total_terms: all terms in the boolean query
+    """
+    def _tokenize(self, query_infos):
+        total_terms = set()
+
+        for query_info in query_infos:
+            query = query_info.query
+
+            # step 1: tokenize the query string
+            tokens = [
+                word for sent in nltk.sent_tokenize(query)
+                for word in nltk.word_tokenize(sent)
+            ]
+
+            # step 2: stem the tokens
+            tokens = [self.stemmer.stem(token.lower()) for token in tokens]
+
+            # step 3: get the term count
+            term_count = defaultdict(lambda: 0)
+            for token in tokens:
+                term_count[token] += 1
+
+            # step 4: get terms and counts
+            terms = []
+            counts = []
+            for term in term_count:
+                terms.append(term)
+                counts.append(term_count[term])
+
+            # step 5: update query info
+            query_info.terms = terms
+            query_info.counts = counts
+            query_info.tokens = tokens
+
+            # step 6: update total_terms
+            total_terms.update(terms)
+
+        # step 7: return total_terms
+        return total_terms
 
     """ Get the query vector based on the postings_lists
 
     Args:
         query_infos: the list of query info
-        postings_lists: the dictionary with terms to posting lists mapping
+        postings_lists: the dictionary with terms to postings lists mapping
     """
     def _get_query_vector(self, query_infos, postings_lists):
         # N = len(self.indexer.total_doc) + 1
@@ -217,10 +259,16 @@ if __name__ == '__main__':
         'comput'  : (np.array([0, 3, 6, 9]), np.array([1, 3, 6, 9]), [np.array([7, ])] )
     }
 
-    test = '_get_query_vector'
+    test = 'refine' if len(sys.argv) == 1 else sys.argv[1]
 
-    if test == 'split':
-        query_infos, total_terms = refiner._split_query(query)
+    if test == '_split_query':
+        query_infos = refiner._split_query(query)
+        for query_info in query_infos:
+            print("query: ", query_info.query)
+            print("is_phrase: ", query_info.is_phrase)
+    elif test == '_tokenize':
+        query_infos = refiner._split_query(query)
+        total_terms = refiner._tokenize(query_infos)
         print(total_terms)
         for query_info in query_infos:
             print("query: ", query_info.query)
@@ -229,7 +277,14 @@ if __name__ == '__main__':
             print("counts: ", query_info.counts)
             print("tokens: ", query_info.tokens)
     elif test == '_get_query_vector':
-        query_infos, total_terms = refiner._split_query(query)
+        query_infos = refiner._split_query(query)
+        total_terms = refiner._tokenize(query_infos)
         refiner._get_query_vector(query_infos, postings_lists)
         for query_info in query_infos:
             print("query_vector", query_info.query_vector)
+    elif test == 'refine':
+        query_infos, _ = refiner.refine(query, [0])
+        for query_info in query_infos:
+            print("query: ", query_info.query)
+            print("is_phrase: ", query_info.is_phrase)
+            print("query_vector: ", query_info.query_vector)
